c4107b865c546b79affcd1d299539feef9644667
Aleksey <alex@uptrade.com>
2024-08-20 17:58:43 +0300

    move default api route to controller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::middleware('auth:api')->group(function(){
+    Route::get('/user', [ApiController::class, 'user']);
+});
